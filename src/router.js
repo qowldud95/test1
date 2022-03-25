a84a317311d4c55e4955b4a35c664ac619fe0d1a
qowldud95 <girlwldud@naver.com>
2022-03-25 20:54:29 +0900

    vue2

---
+++ src/router.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "./views/Home";
+import About from "./views/About";
+
+Vue.use(VueRouter);
+
+const router = new VueRouter ({
+    mode: "history",
+    routes: [
+        {path:"/", component: Home}, //기본path
+        {path:"/about", component: About}
+    ]
+});
+export default router;
